0594359027bddcd8300e357ced20c63210cc9981
Angel Erick Cruz Olivera <leganux@hotmail.com>
2023-07-20 22:50:01 -0600

    env

--- config/db.py
+++ config/db.py
@@ -1,6 +1,13 @@
 from pymongo import MongoClient
+import os
 
-db_connection = MongoClient("mongodb://localhost:27017")
+from dotenv import load_dotenv
+load_dotenv()
+
+
+MONGODB = os.getenv("MONGODB")
+print(MONGODB)
+db_connection = MongoClient(MONGODB)
 db = db_connection.king_tide
 collection = db["user"]
 collection_files = db["files"]
